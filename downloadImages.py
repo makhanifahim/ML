b0d63076d98c5c93494020a784e71c066e000fec
Fahim <fahimmakhanifm@gmail.com>
2022-05-20 10:13:57 +0530

    Create downloadImages.py

---
+++ downloadImages.py
@@ -0,0 +1,4 @@
+from bing_image_downloader import downloader
+downloader.download("knife",limit=50,output_dir='dataa')
+downloader.download("fork",limit=50,output_dir='dataa')
+downloader.download("spoon",limit=50,output_dir='dataa')
